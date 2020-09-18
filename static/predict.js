cea0b08cbec2ece9b248d471732d3f704d7282f4
Olga.L <olg200492@gmail.com>
2020-09-18 21:26:35 +0300

    updates

--- static/predict.js
+++ static/predict.js
@@ -19,7 +19,7 @@ $( document ).ready(async function () {
 	modelLoaded = false;
 	$('.progress-bar').show();
     console.log( "Loading model..." );
-    model = await tf.loadGraphModel('model/model.json');
+    model = await tf.loadLayersModel("model/model.js");
     console.log( "Model loaded." );
 	$('.progress-bar').hide();
 	modelLoaded = true;
@@ -34,7 +34,7 @@ $("#predict-button").click(async function () {
 	// Pre-process the image
 	console.log( "Loading image..." );
 	let tensor = tf.browser.fromPixels(image, 3)
-		.resizeNearestNeighbor([224, 224]) // change the image size
+		.resizeNearestNeighbor([64, 64]) // change the image size
 		.expandDims()
 		.toFloat()
 		.reverse(-1); // RGB -> BGR
